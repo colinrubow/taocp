8bb70722c28ae228959c8519c11d93512265cc66
Colin Rubow <colinwrubow@gmail.com>
2026-03-22 20:12:40 -0600

    start on inverse

---
+++ programs/permutations/inverse.py
@@ -0,0 +1,9 @@
+def inverse_i(perm: list[int]) -> None:
+    """
+    TAOCP 1.3.3 finds the inverse permutation, in place
+    """
+
+if __name__ == '__main__':
+    perm = [5, 1, 0, 4, 3, 2]
+    inverse_i(perm)
+    print(perm)
